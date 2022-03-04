aec5ae7466b6f90574501aed18c02b9797c80cdc
ggichuru <ggichuru.dev@gmail.com>
2022-03-04 21:14:05 +0300

    Counter rust

--- programs/solana_one/src/lib.rs
+++ programs/solana_one/src/lib.rs
@@ -8,5 +8,34 @@ pub mod solana_one {
 
     pub fn create(ctx: Context<Create>) -> ProgramResult {
         let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
     }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        Ok(())
+    }
+}
+// Account that goes in inside a transaction instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
+
+// Transaction instructions
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
 }
